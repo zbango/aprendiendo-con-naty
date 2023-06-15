3d404792eea6d7583f8cd56895c27758b4680b5f
Steven Tabango <steven.tabango@gmail.com>
2023-06-14 22:01:24 -0500

    Clase 1

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 console.log("Hola Naty")
+// VARIABLES
+
+// Constantes
+const diaDePago = 30
+console.log("El valor de diaDePago es", diaDePago)
+
+// Variable
+let diaDePagoVariable = 30
+console.log("El valor de diaDePagoVariable es", diaDePagoVariable)
+diaDePagoVariable = 28
+console.log("El valor de diaDePagoVariable es", diaDePagoVariable)
+diaDePagoVariable = 30
+console.log("El valor de diaDePagoVariable es", diaDePagoVariable)
+
+// Tipos de datos
+let number = 3
+console.log(typeof (number))
+let numberB = 3.6
+console.log(typeof (numberB))
+let text = "3"
+console.log(typeof (text))
+let logic = false
+console.log(typeof (logic))
+let array = [1, 3.6, "Hola", false]
+console.log(typeof (array))
+let object = {
+    nombre: "Naty",
+    edad: 33
+}
+console.log(typeof (object))
+
+// OPERADORES
+// = asignar un valor a una variable
+// == comparar una igualdad
+// >= mayor o igual
+// <= menor o igual
+// > mayor
+// < menor
+// !== distinto a 
+// && AND
+// || OR
+// * multiplicar
+// - restar
+// + sumar
+// / dividir
+// // comentar
+
+
+// CONDICIONALES
+if (3 + 3 !== 1) {
+    // LOGICA SI SE CUMPLE
+    console.log("La expresion es verdadera")
+}else{
+    // LOGICA SI NO SE CUMPLE
+    console.log("La expresion es falsa")
+}
